ffaca4be84021fdda13b631793570cf200655a7f
James Spencer <e.spencer@kyspencers.net>
2023-09-28 18:27:50 +0000

    edits

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,9 @@
 
-const 
+const
 
 
 
-const zipCodeInput =document.getElementById('inputZip');
+const zipCodeInput = document.getElementById('inputZip');
 const submitBtn = document.getElementById('zipBtn');
 zipCodeInput.addEventListener("input", updateValue);
 
@@ -12,18 +12,19 @@ function updateValue(e) {
     zipCodeInput.value = e.target.value
 }
 
-const apiKey = '9f828be37dae1608f383c3381afd374a'; 
+const apiKey = '9f828be37dae1608f383c3381afd374a';
 
 
 submitBtn.addEventListener('click', () => {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCodeInput.value}&appid=${apiKey}`;
     console.log(apiUrl);
-axios.get(apiUrl)
-  .then(response => {
-    const weatherData = response.data
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
- })})
+    axios.get(apiUrl)
+        .then(response => {
+            const weatherData = response.data
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        })
+})
 
